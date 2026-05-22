82487657945bacd08b23df6e4e465f47d4d211c3
Ishta18 <ishta1286@gmail.com>
2026-05-22 22:45:05 +0530

    Update adv_app.py

--- adv_app.py
+++ adv_app.py
@@ -1,4 +1,4 @@
-import stremlit as st
+import streamlit as st
 import pickle
 import numpy as np
 model = pickle.load(open('lr.sav','rb'))
@@ -10,4 +10,4 @@ Newspaper=st.number_input('Newspaper adv budget', min_value=0.0)
 if st.button('predct sales'):
   input_data=np.array([[TV,Radio,Newspaper]])
   predction=model.predict(input_data)[0]
-  st.success(f'predicted sales: {Prediction:.2f)')
+  st.success(f'predicted sales: {Prediction:.2f}')
